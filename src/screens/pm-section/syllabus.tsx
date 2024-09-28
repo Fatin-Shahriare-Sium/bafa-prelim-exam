e73495f38f2ef503b7f293ae73a2ec049219ca8f
unknown <sium1206@gmail.com>
2024-09-28 23:19:42 +0600

    alhamdulillah,done phyMath section

--- src/screens/pm-section/syllabus.tsx
+++ src/screens/pm-section/syllabus.tsx
@@ -1,14 +1,32 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import SingleSyllabus from '../../components/singleSyllabus';
 
 const PmSyllabus = () => {
     return (
-        <View>
-            <Text>PmSyllabus</Text>
+        <View style={styles.PMSyllabusWrapper}>
+            <Text style={{textAlign: 'center', fontSize: 30, color: 'white', fontWeight: '700', margin: 9}}>🎯 PhyMath Topics</Text>
+            <SingleSyllabus text="1.😴 Just First Paper of Both ⭐⭐⭐⭐⭐" />
+            <SingleSyllabus text="2.Vector ⭐⭐⭐⭐⭐" />
+            <SingleSyllabus text="3.Newtonian mechanics⭐⭐⭐⭐⭐" />
+            <SingleSyllabus text="4.Work and energy ⭐⭐  " />
+            <SingleSyllabus text="5.Lose and profit math ⭐⭐⭐⭐⭐" />
+            <SingleSyllabus text="6.Diffrention  ⭐⭐⭐" />
+            <SingleSyllabus text="7.Intregation ⭐" />
+            <SingleSyllabus text="8.Others topics within only first paper ⭐⭐⭐" />
         </View>
     );
 };
 
 export default PmSyllabus;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    PMSyllabusWrapper: {
+        backgroundColor: 'black',
+        display: 'flex',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width,
+    },
+});
--- src/screens/pm-section/tips.tsx
+++ src/screens/pm-section/tips.tsx
@@ -1,14 +1,27 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const PmTips = () => {
     return (
-        <View>
-            <Text>PmTips</Text>
+        <View style={styles.pmTipsWrapper}>
+            <Text style={{textAlign: 'center', fontSize: 30, color: 'white', fontWeight: '700', margin: 9}}>💡 PhyMath Tips</Text>
+            <View>
+                <Text style={{textAlign: 'left', fontSize: 23, color: 'white', fontWeight: '500', marginTop: '30%', marginLeft: '3%'}}>
+                    Phy+Math exam is not that much easy.Here,approximately 7-10 candidates disqualified.Math and Physics exam will be held in one question paper.25 math +25 Physics questions in 25 minutes<Text style={{color: 'red', fontWeight: 'bold'}}>You can not use calculator here.</Text>
+                    <Text>You should practise popular mcq of Physics and Math that frequently comes in H.S.C examination.</Text>
+                    <Text style={{color: '#3ee33c', fontWeight: 'bold'}}>The most important thing is that you should study only First paper of Physics and Math as the maximum questions come from first paper not from second paper</Text>
+                </Text>
+            </View>
         </View>
     );
 };
 
 export default PmTips;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    pmTipsWrapper: {
+        height: Dimensions.get('window').height,
+        backgroundColor: 'black',
+    },
+});
