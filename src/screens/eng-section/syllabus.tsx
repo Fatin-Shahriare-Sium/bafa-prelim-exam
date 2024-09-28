5d56f71fa8ca26903b68ac48c7ebbe3992ad14f8
unknown <sium1206@gmail.com>
2024-09-28 22:04:21 +0600

    alhamdulillah,done eng exam section design

--- src/screens/eng-section/syllabus.tsx
+++ src/screens/eng-section/syllabus.tsx
@@ -1,14 +1,32 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import SingleSyllabus from '../../components/singleSyllabus';
 
 const EngSyllabus = () => {
     return (
-        <View>
-            <Text>EngSyllabus</Text>
+        <View style={styles.EngSyllabusWrapper}>
+            <Text style={{textAlign: 'center', fontSize: 30, color: 'white', fontWeight: '700', margin: 9}}>🎯 English Topics</Text>
+            <SingleSyllabus text="1.Parts of Speech ⭐⭐⭐ " />
+            <SingleSyllabus text="2.Identify Parts of Speech  ⭐⭐" />
+            <SingleSyllabus text="3.Tense ⭐ " />
+            <SingleSyllabus text="4.Narration(Indirect/Direct speech conversion) ⭐⭐⭐⭐⭐" />
+            <SingleSyllabus text="5.Tag Questions ⭐⭐⭐" />
+            <SingleSyllabus text="6.Transformation of sentences ⭐⭐⭐⭐" />
+            <SingleSyllabus text="7.Clauses ⭐" />
+            <SingleSyllabus text="8.Voice ⭐⭐⭐⭐" />
         </View>
     );
 };
 
 export default EngSyllabus;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    EngSyllabusWrapper: {
+        backgroundColor: 'black',
+        display: 'flex',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width,
+    },
+});
--- src/screens/eng-section/tips.tsx
+++ src/screens/eng-section/tips.tsx
@@ -1,14 +1,23 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const EngTips = () => {
     return (
-        <View>
-            <Text>EngTips</Text>
+        <View style={styles.engTipsWrapper}>
+            <Text style={{textAlign: 'center', fontSize: 30, color: 'white', fontWeight: '700', margin: 9}}>💡 Englsih Tips</Text>
+            <View>
+                <Text style={{textAlign: 'left', fontSize: 23, color: 'white', fontWeight: '500', marginTop: '30%', marginLeft: '3%'}}>English exam is pretty much easier.Only,50 mcq in 30 minutes.Usually,2-5 candidates disqualify in english exam. The question usually comes from basic english grammer. If anyone has good basic knowledge about english,this exam will be very much easier surely</Text>
+            </View>
         </View>
     );
 };
 
 export default EngTips;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    engTipsWrapper: {
+        height: Dimensions.get('window').height,
+        backgroundColor: 'black',
+    },
+});
